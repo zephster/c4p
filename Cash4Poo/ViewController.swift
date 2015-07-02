43e1277efd51d794f6b33612e5bfd5d5d78a99f2
Brandon <antizeph@gmail.com>
2015-07-01 22:05:50 -0400

    Initial Commit

---
+++ Cash4Poo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cash4Poo
+//
+//  Created by brandon on 7/1/15.
+//  Copyright (c) 2015 cbcoding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

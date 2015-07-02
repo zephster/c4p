b6ef68ced052b1e59b39687fe041c7f826ccf0d8
Brandon <antizeph@gmail.com>
2015-07-01 23:01:31 -0400

    oh man, all kinds of watch stuff. still no actual code. progress!

---
+++ Cash4Poo WatchKit Extension/GlanceController.swift
@@ -0,0 +1,31 @@
+//
+//  GlanceController.swift
+//  Cash4Poo WatchKit Extension
+//
+//  Created by brandon on 7/1/15.
+//  Copyright (c) 2015 cbcoding. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}

327cf2cce9618c6ac8cd0d2db934ea0c489221db
Brandon <antizeph@gmail.com>
2015-07-03 12:31:12 -0400

    stopwatch code whee

--- Cash4Poo WatchKit Extension/InterfaceController.swift
+++ Cash4Poo WatchKit Extension/InterfaceController.swift
@@ -11,6 +11,12 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
+    @IBOutlet weak var lblStopwatch: WKInterfaceLabel!
+    @IBOutlet weak var btnStart: WKInterfaceButton!
+    @IBOutlet weak var btnStop: WKInterfaceButton!
+    
+    var timer = NSTimer()
+    var startTime = NSTimeInterval()
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
@@ -26,6 +32,68 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+
+    @IBAction func startPooping()
+    {
+        print("tapped start button in watch app\n")
+        
+        // start if timer is stopped
+        if (!self.timer.valid)
+        {
+            self.timer = NSTimer.scheduledTimerWithTimeInterval(
+                0.1,
+                target: self,
+                selector: "poopTick",
+                userInfo: nil,
+                repeats: true
+            )
+            
+            self.startTime = NSDate.timeIntervalSinceReferenceDate()
+            
+            self.btnStart.setEnabled(false)
+        }
+    }
+    
+    
+    @IBAction func stopPooping()
+    {
+        print("tapped stop button in watch app\n")
+        self.timer.invalidate()
+        self.btnStart.setEnabled(true)
+    }
+    
+    func poopTick()
+    {
+        var currentTime = NSDate.timeIntervalSinceReferenceDate()
+        
+        //Find the difference between current time and start time.
+        
+        var elapsedTime = currentTime - self.startTime
+        
+        //calculate the minutes in elapsed time.
+        
+        let minutes = UInt8(elapsedTime / 60.0)
+        
+        elapsedTime -= (NSTimeInterval(minutes) * 60)
+        
+        //calculate the seconds in elapsed time.
+        
+        let seconds = UInt8(elapsedTime)
+        
+        elapsedTime -= NSTimeInterval(seconds)
+        
+        //find out the fraction of milliseconds to be displayed.
+        
+        let fraction = UInt8(elapsedTime * 100)
+        
+        //add the leading zero for minutes, seconds and millseconds and store them as string constants
+        
+        let strMinutes = String(format: "%02d", minutes)
+        let strSeconds = String(format: "%02d", seconds)
+        let strFraction = String(format: "%02d", fraction)
+        
+        self.lblStopwatch.setText("\(strMinutes):\(strSeconds):\(strFraction)")
     }
 
 }
--- Cash4Poo/ViewController.swift
+++ Cash4Poo/ViewController.swift
@@ -8,7 +8,62 @@
 
 import UIKit
 
+func timeIntervalToString(ti: NSTimeInterval) -> String? {
+    let dcf = NSDateComponentsFormatter()
+    dcf.zeroFormattingBehavior = .Pad
+    dcf.allowedUnits = (.CalendarUnitHour | .CalendarUnitMinute | .CalendarUnitSecond | .CalendarUnitNanosecond)
+    return dcf.stringFromTimeInterval(ti)
+}
+
+public struct StopWatch {
+    private var startTime: NSDate?
+    private var accumulatedTime: NSTimeInterval = 0.0
+    
+    public var elapsedTimeInterval: NSTimeInterval {
+        get {
+            return self.accumulatedTime + NSDate().timeIntervalSinceDate(self.startTime ?? NSDate())
+        }
+    }
+    
+    public var elapsedTimeString: String {
+        get {
+            return timeIntervalToString(self.elapsedTimeInterval) ?? "00:00:00"
+        }
+    }
+    
+    public mutating func start() {
+        self.startTime = NSDate()
+    }
+    
+    public mutating func stop() {
+        self.accumulatedTime += NSDate().timeIntervalSinceDate(self.startTime ?? NSDate())
+        self.startTime = nil
+    }
+    
+    public mutating func reset() {
+        self.accumulatedTime = 0.0
+        self.start()
+    }
+}
+
+
+
+
+
+
+
+
+
 class ViewController: UIViewController {
+    @IBOutlet weak var lblStopwatch: UILabel!
+    @IBOutlet weak var btnStart: UIButton!
+    @IBOutlet weak var btnStop: UIButton!
+    
+    var startTime = NSTimeInterval()
+    
+    var stopWatch: StopWatch = StopWatch()
+    var timer: NSTimer?
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +74,84 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func updatePoopUI()
+    {
+        self.lblStopwatch.text = self.stopWatch.elapsedTimeString
+        print("updatePoopUI()\n")
+    }
 
 
+    @IBAction func startPooping(sender: UIButton)
+    {
+        print("tapped start button in main app\n")
+        
+        if (self.timer?.valid == nil ||
+            self.timer?.valid == false)
+        {
+            self.timer = NSTimer.scheduledTimerWithTimeInterval(
+                0.10,
+                target: self,
+                selector: "updatePoopUI",
+                userInfo: nil,
+                repeats: true
+            )
+
+            self.btnStart.enabled = false
+            self.btnStop.enabled = true
+            self.stopWatch.start()
+        }
+    }
+    
+    @IBAction func stopPooping(sender: UIButton)
+    {
+        print("tapped stop button in main app\n")
+        self.stopWatch.stop()
+        self.timer?.invalidate()
+        self.btnStart.enabled = true
+        
+        if (self.timer?.valid == nil ||
+            self.timer?.valid == false)
+        {
+            print("reset poopwatch")
+            self.stopWatch.reset()
+            updatePoopUI()
+        }
+    }
+    
+    
+    func poopTick()
+    {
+        var currentTime = NSDate.timeIntervalSinceReferenceDate()
+        
+        //Find the difference between current time and start time.
+        
+        var elapsedTime = currentTime - self.startTime
+        
+        //calculate the minutes in elapsed time.
+        
+        let minutes = UInt8(elapsedTime / 60.0)
+        
+        elapsedTime -= (NSTimeInterval(minutes) * 60)
+        
+        //calculate the seconds in elapsed time.
+        
+        let seconds = UInt8(elapsedTime)
+        
+        elapsedTime -= NSTimeInterval(seconds)
+        
+        //find out the fraction of milliseconds to be displayed.
+        
+        let fraction = UInt8(elapsedTime * 100)
+        
+        //add the leading zero for minutes, seconds and millseconds and store them as string constants
+        
+        let strMinutes = String(format: "%02d", minutes)
+        let strSeconds = String(format: "%02d", seconds)
+        let strFraction = String(format: "%02d", fraction)
+        
+        self.lblStopwatch.text = "\(strMinutes):\(strSeconds):\(strFraction)"
+    }
 }
 
